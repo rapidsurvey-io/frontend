dcdb88be22309ac2eaf9eb816aa69d8712d9d38c
Thomas William McClean <spam@tommcclean.me>
2020-04-16 22:16:05 +0100

    Add Button Component

---
+++ components/shared/button.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="c-button" v-bind:class="[{ 'u-primary': isPrimary }]">
+    <span v-text="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'Submit'
+    },
+    isPrimary: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.c-button {
+  display: inline-block;
+  padding: 15px 20px;
+  background-color: #B2CDFF;
+  cursor: pointer;
+
+  &.u-primary {
+    background-color: #003FBA;
+    color: white;
+  }
+}
+</style>
